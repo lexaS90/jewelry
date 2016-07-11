4bb29f6a2a79fbb14f9e12d4024c3336adb2381a
Alexey Stupakov <workinweb24@gmail.com>
2016-07-11 14:31:18 +0300

    header height

--- app/js/main.js
+++ app/js/main.js
@@ -14,8 +14,43 @@ $(document ).ready(function(){
 
 	/* ====== End video ====== */
 
+	/* ====== Height element ====== */
+
+	headerH();
+	
+
+	/* ====== End height element ====== */
+
+});
+
+$(window).resize(function(){
+	headerH();
+
 });
 
 $(window).scroll(function(){
 
 });
+
+function headerH(){
+	function get_bottom_coordinate(ob){
+		var $el = ob; 
+		return $el.offset().top + $el.outerHeight(true);
+	}
+	
+	if ($("html").width() > 992){
+		var html_h = $("html").height();
+		$(".main-header-left").height(html_h);
+	}
+	else{
+		$(".main-header-left").height("auto");
+	}
+
+	var top = get_bottom_coordinate($(".img-jewelry-wrapper"));
+	var bottom = get_bottom_coordinate($(".main-header"));
+
+	var form_w = $(".form-jewelry-wrapper").height();
+
+	var form_top_margin = (bottom - top - form_w) / 2;
+	$(".form-jewelry-wrapper").css("margin-top", form_top_margin);
+}
